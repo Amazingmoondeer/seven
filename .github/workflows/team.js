e0839eca16a2eab265110c502b454b2c7e58f614
Amazingmoondeer <deuswambiji07@gmail.com>
2025-07-01 10:59:10 +0300

    Create team.js

---
+++ .github/workflows/team.js
@@ -0,0 +1,7 @@
+const teamsData = {
+  "Argentina": { wins: 15, losses: 3 },
+  "Brazil": { wins: 14, losses: 4 },
+  "France": { wins: 13, losses: 5 },
+  "Germany": { wins: 10, losses: 8 },
+  "Spain": { wins: 11, losses: 7 }
+};
